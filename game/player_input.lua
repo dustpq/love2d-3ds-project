86f327182bc0911cdfb11ea4e97c33c1b41e0b99
dust.mac <portillo0121@gmail.com>
2026-03-02 22:46:40 -0500

    redid input binding and set up

--- game/player_input.lua
+++ game/player_input.lua
@@ -2,33 +2,80 @@ package.path = package.path .. ";libs/?.lua;libs/?/init.lua"
 
 local baton = require("baton")
 
-local gameInput = baton.new {
-    controls = {
-        left = {'axis:leftx-', 'dpleft'},
-        right = {'axis:leftx+', 'dpright'},
-        up = {'axis:lefty-', 'dpup'},
-        down = {'axis:lefty+', 'dpdown'},
+local bindings = {
 
-    },
+    --3DS controls (hopefully)
+    analogLeft = {'axis:leftx-'},
+    analogRight = {'axis:leftx+'},
+    analogUp = {'axis:lefty-'},
+    analogDown = {'axis:lefty+'},
+
+    dpLeft = {'button:dpleft'},
+    dpRight = {'button:dpright'},
+    dpUp = {'button:dpup'},
+    dpDown = {'button:dpdown'},
+
+    buttonA = {'button:a'},
+    buttonB = {'button:b'},
+    buttonX = {'button:x'},
+    buttonY = {'button:y'},
+
+    shoulderR = {'button:shoulderr'},
+    shoulderL = {'button:shoulderl'},
+
+    buttonStart = {'button:start'},
+    buttonSelect = {'button:select'},
+
+    --New 3DS controls
+    buttonZR = {'button:triggerright'},
+    buttonZL = {'button:triggerleft'},
+
+    --Keyboard bindings (for testing on PC)
+    keyLeft = {'key:a'},
+    keyRight = {'key:d'},
+    keyUp = {'key:w'},
+    keyDown = {'key:s'},
+
+    keyA = {'key:k'},
+    keyB = {'key:m'},
+    keyX = {'key:j'},
+    keyY = {'key:i'},
+
+    keyR = {'key:q'},
+    keyL = {'key:e'}
+
+}
+
+local gameInput = baton.new {
+    controls = bindings,
     pairs = {
-        rStick = {},
-        dpad = {},
-        faceButtons = {}
+        left = {'analogLeft', 'dpLeft', 'keyLeft'},
+        right = {'analogRight', 'dpRight', 'keyRight'},
+        up = {'analogUp', 'dpUp', 'keyUp'},
+        down = {'analogDown', 'dpDown', 'keyDown'},
+
+        move = {'left', 'right', 'up', 'down'},
+        action = {'buttonB', 'keyB'},
+        slow = {'buttonX', 'keyX'},
     },
-    joystick = love.joystick.getJoysticks()[1] -- Get the first joystick (3DS controls)
+    joystick = love.joystick.getJoysticks()[1], -- Get the first joystick (3DS controls)
+    deadzone = .25, -- Set a deadzone for analog inputs
 }
 
 local menuInput = baton.new {
-    controls = {
-        left = {'axis:leftx-', 'dpleft'},
-        right = {'axis:leftx+', 'dpright'},
-        up = {'axis:lefty-', 'dpup'},
-        down = {'axis:lefty+', 'dpdown'}
-    },
+    controls = bindings,
     pairs = {
-        dpad = {}
+        left = {'analogLeft', 'dpLeft', 'keyLeft'},
+        right = {'analogRight', 'dpRight', 'keyRight'},
+        up = {'analogUp', 'dpUp', 'keyUp'},
+        down = {'analogDown', 'dpDown', 'keyDown'},
+
+        move = {'left', 'right', 'up', 'down'},
+        confirm = {'buttonA', 'keyA'},
+        cancel = {'buttonB', 'keyB'},
     },
-    joystick = love.joystick.getJoysticks()[1] -- Get the first joystick (3DS controls)
+    joystick = love.joystick.getJoysticks()[1], -- Get the first joystick (3DS controls)
+    deadzone = .33, -- Set a deadzone for analog inputs
 }
 
 function ReturnPlayerInput(player)
