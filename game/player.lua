00148fefce05a73d5a956d736a792dae128baf13
dust.mac <portillo0121@gmail.com>
2026-03-02 22:46:52 -0500

    redoing update loop

--- game/player.lua
+++ game/player.lua
@@ -1,29 +1,38 @@
 require("player_input") -- Load player input handling
 
+local function handle_movement(player, dt)
+    if player.input:down('move') then
+        local moveX, moveY = player.input:get('move')
+        player.x = player.x + moveX * player.speed * dt
+        player.y = player.y + moveY * player.speed * dt
+    end
+end
+
+local function handle_menu_navigation(player, dt)
+    -- Implement menu navigation logic here using player.input
+end
+
+local function checkInput(player)
+    player.input = ReturnPlayerInput(player)
+end
+
 local player = {
     sprite = nil,
     x = 100,
     y = 100,
     speed = 200,
-    state="game",
+    state = "game",
     input = nil,
-    update=function(self, dt)
-        checkInput(self) -- Update the player's input based on their current state
-        if self.input.left then
-            self.x = self.x - self.speed * dt
-        end
-        if self.input.right then
-            self.x = self.x + self.speed * dt
-        end
-        if self.input.up then
-            self.y = self.y - self.speed * dt
-        end
-        if self.input.down then
-            self.y = self.y + self.speed * dt
+    update = function(player, dt)
+        checkInput(player) -- Update the player's input based on their current states
+        if player.state == "game" then
+            handle_movement(player, dt)
+        elseif player.state == "menu" then
+            handle_menu_navigation(player, dt)
         end
     end,
-    draw=function(self)
-        love.graphics.draw(self.sprite, self.x, self.y)
+    draw = function(player)
+        love.graphics.draw(player.sprite, player.x, player.y)
     end
 }
 
@@ -32,8 +41,4 @@ function AddPlayer()
         player.sprite = love.graphics.newImage("assets/images/SOUL.png")
     end
     return player
-end
-
-function checkInput(player)
-    player.input = ReturnPlayerInput(player)
 end
